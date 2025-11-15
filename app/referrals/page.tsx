88b12f7fdfd4fca5f13477385e5e3a5ec581c159
placeparks <86215469+placeparks@users.noreply.github.com>
2025-11-16 00:51:13 +0500

    Update page.tsx

--- app/referrals/page.tsx
+++ app/referrals/page.tsx
@@ -335,6 +335,7 @@ export default function ReferralsPage() {
               text={referralShareText}
               pageUrl={data.referralLink}
               imageUrl={referralShareImage}
+              linkEmbed={false}
             />
           </div>
         )}
@@ -723,5 +724,4 @@ export default function ReferralsPage() {
     </>
   );
 }
-
 
